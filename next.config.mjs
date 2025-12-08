eb23b096bdcbf3b735d6f005bf6f902526fc97bb
Lukas Herman <lherman.cs@gmail.com>
2025-12-08 15:35:01 -0500

    redirect to docs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const withMDX = createMDX();
 const config = {
   reactStrictMode: true,
   output: 'export',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/docs',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
